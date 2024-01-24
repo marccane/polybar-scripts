6aa404eeea2667f89998d700aafdf0691f822f34
Marc Cane <marc00077@hotmail.com>
2024-01-24 23:03:33 +0100

    Add fronius script, adapt system-cpu-temppercore.sh to my preferences

---
+++ polybar-scripts/fronius/fronius.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+import json
+import urllib.request
+
+def zin(n): #zero if None
+	return 0 if n is None else n
+
+ip="192.168.1.76"
+url="http://"+ip+"/solar_api/v1/GetPowerFlowRealtimeData.fcgi"
+try:
+	contents = urllib.request.urlopen(url).read()
+	root = json.loads(contents)
+	site = root["Body"]["Data"]["Site"]
+
+	pv, pgrid, pload, scons, autonomy = site["P_PV"], site["P_Grid"], site["P_Load"], site["rel_SelfConsumption"], site["rel_Autonomy"]
+	pv, pgrid, pload, scons, autonomy = zin(pv), zin(pgrid), zin(pload), zin(scons), zin(autonomy)
+	pv, pgrid, pload, scons, autonomy = int(pv), float(pgrid), float(pload), float(scons), float(autonomy)
+	pload = abs(pload)
+	#feed =  #"regalem", no es trivial de calcular
+
+	# generat consumit %autonomy
+	print("PV %dW %.1fW %.1f%%" % (pv, pload, autonomy))
+except: 
+	print("Error")
--- polybar-scripts/system-cpu-temppercore/system-cpu-temppercore.sh
+++ polybar-scripts/system-cpu-temppercore/system-cpu-temppercore.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-sensors | grep Core | awk '{print substr($3, 2, length($3)-5)}' | tr "\\n" " " | sed 's/ /°C  /g' | sed 's/  $//'
+#sensors | grep CORE | awk '{print substr($3, 2, length($3)-5)}' | tr "\\n" " " | sed 's/ /°C  /g' | sed 's/  $//'
+
+xapussa10=`sensors`
+xapussa9=$xapussa10
+xapussa9="${xapussa10//.0}"
+xapussa="${xapussa9//+}"
+
+cpu=$(echo "$xapussa" | grep CPUTIN | tr -s " " | cut -d' ' -f2)
+#sys=$(echo "$xapussa" | grep SYSTIN | tr -s " " | cut -d' ' -f2)
+aux=$(echo "$xapussa" | grep AUXTIN0 | tr -s " " | cut -d' ' -f2)
+#aux2=$(echo "$xapussa" | grep AUXTIN2 | tr -s " " | cut -d' ' -f2)
+sbus=$(echo "$xapussa" | grep SMBUSMASTER | tr -s " " | cut -d' ' -f3)
+gpu=$(echo "$xapussa" | grep edge: | tr -s " " | cut -d' ' -f2)
+
+echo "CPU ${cpu}C GPU ${gpu}C"
+#AUX ${aux}C SBUS ${sbus}C
